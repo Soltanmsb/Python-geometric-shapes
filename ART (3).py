94aa5165c01ee16114f8094709ef8181b967e219
MSB <103173374+Soltanmsb@users.noreply.github.com>
2023-07-12 17:15:32 +0330

    Add files via upload

---
+++ ART (3).py
@@ -0,0 +1,19 @@
+from turtle import*
+import colorsys
+bgcolor('black')
+tracer(15)
+
+width(2)
+right(15)
+h = 0
+for i in range(300):
+    c = colorsys.hsv_to_rgb(h,1,1)
+    h +=0.005
+    pencolor(c)
+    fillcolor('black')
+    begin_fill()
+    for j in range(2):
+        fd(i*3)
+        rt(1)
+        rt(60)
+done()
---
+++ ART (4).py
@@ -0,0 +1,18 @@
+from turtle import*
+import colorsys
+bgcolor('black')
+tracer(15)
+
+width(2)
+h = 0
+for i in range(300):
+    c = colorsys.hsv_to_rgb(h,1,1)
+    h +=0.005
+    pencolor(c)
+    fillcolor('black')
+    begin_fill()
+    for j in range(2):
+        fd(i*1.2)
+        fd(250)
+        rt(121)
+done()
